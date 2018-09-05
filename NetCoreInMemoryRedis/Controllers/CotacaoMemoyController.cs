2c95aae7784317e935b7a436ae109a1579a6c2a7
BNK SYSTEM <bnksystem@outlook.com>
2018-09-05 15:29:32 -0300

    Include application Asp.NetCore - InMemory or Redis.

---
+++ NetCoreInMemoryRedis/Controllers/CotacaoMemoyController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Data.SqlClient;
+using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Configuration;
+
+namespace NetCoreInMemoryOrRedis.Controllers
+{
+    public class CotacaoMemoyController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ContentResult Get(
+    [FromServices]IConfiguration config,
+    [FromServices]IMemoryCache cache)
+        {
+            string valorJSON = cache.GetOrCreate<string>(
+                "Cotacoes", context =>
+                {
+                    context.SetAbsoluteExpiration(TimeSpan.FromSeconds(30));
+                    context.SetPriority(CacheItemPriority.High);
+
+                    using (SqlConnection conexao = new SqlConnection(
+                        config.GetConnectionString("BaseCotacoes")))
+                    {
+                        return conexao.QueryFirst<string>(
+                            "SELECT Sigla " +
+                                  ",NomeMoeda " +
+                                  ",UltimaCotacao " +
+                                  ",GETDATE() AS DataProcessamento " +
+                                  ",ValorComercial AS 'Cotacoes.Comercial' " +
+                                  ",ValorTurismo AS 'Cotacoes.Turismo' " +
+                            "FROM dbo.Cotacoes " +
+                            "ORDER BY NomeMoeda " +
+                            "FOR JSON PATH, ROOT('Moedas')");
+                    }
+                });
+
+            return Content(valorJSON, "application/json");
+        }
+    }
+}
---
+++ NetCoreInMemoryRedis/ParamRedisOrMemory.cs
@@ -0,0 +1,8 @@
+﻿namespace NetCoreInMemoryRedis
+{
+    public class ParamRedisOrMemory
+    {
+        public bool Memory { get; set; }
+        public bool Redis { get; set; }
+    }
+}
